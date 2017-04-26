7afa48ac5535f2bee5fc8e14822a98993bfc1405
bhawanapant <upadhyay.bhawana1@gmail.com>
2017-04-26 21:24:27 +0100

    [CHG-000|Bhawana] Dummy commit to check if ssh keys for github works

--- src/test/java/com/esure/sheilas/wheels/steps/pageobjects/HomepageObject.java
+++ src/test/java/com/esure/sheilas/wheels/steps/pageobjects/HomepageObject.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- * Created by bhawana on 23/04/2017.
+ * Created by bhawana-pant on 23/04/2017.
  */
 public class HomepageObject {
     WebDriver aDriver;
