f18eb574aa34509a7f473376d02268105db350dd
bhawanapant <upadhyay.bhawana1@gmail.com>
2017-05-04 09:38:13 +0100

    fixed breaking test

--- src/test/java/com/sheilaswheels/config/ConfigVariables.java
+++ src/test/java/com/sheilaswheels/config/ConfigVariables.java
@@ -1,7 +1,9 @@
 package com.sheilaswheels.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConfigVariables {
 
     @Value("${url}")
